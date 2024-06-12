894dd5243c703d46a1817b6cda6f0bb0d8761aed
gabriel.arocha@edu.tbz.ch <145563844+Gabriel-A-NY@users.noreply.github.com>
2024-06-12 17:04:06 +0200

    make routing better

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 import HomePage from "./Component/pages/Homepage";
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
- 
- 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Menupage from "./Component/pages/Menupage";
+import { Dashboard } from "@material-ui/icons";
+
 function App() {
   return (
     <>
-      
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="home" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/menu" element={<Menupage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dishpreview" /*element={< />}*/ />
+          <Route path="/reservation" /*element={<ReservationPage />}*/ />
         </Routes>
       </BrowserRouter>
     </>
@@ -19,4 +22,3 @@ function App() {
 }
 
 export default App;
-
