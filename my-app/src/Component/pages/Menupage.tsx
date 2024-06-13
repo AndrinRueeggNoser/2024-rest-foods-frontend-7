dafcfaae3930276e8d4271ca9293e6ecb42295e5
gabriel.arocha@edu.tbz.ch <145563844+Gabriel-A-NY@users.noreply.github.com>
2024-06-13 10:40:05 +0200

    implement header in menu page

--- my-app/src/Component/pages/Menupage.tsx
+++ my-app/src/Component/pages/Menupage.tsx
@@ -8,24 +8,28 @@ import {
 } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import Header from "./Elements/Header";
 
 function Menupage() {
   return (
-    <Grid container justifyContent="center">
-      <Grid item xs={12} sm={8} md={6}>
-        <h1>Menu</h1>
+    <>
+      <Header />
+      <Grid container justifyContent="center">
+        <Grid item xs={12} sm={8} md={6}>
+          <h1>Menu</h1>
+        </Grid>
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              food
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              see all
+            </Typography>
+          </CardContent>
+        </Card>
       </Grid>
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Typography variant="h5" component="div">
-            food
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            see all
-          </Typography>
-        </CardContent>
-      </Card>
-    </Grid>
+    </>
   );
 }
 
