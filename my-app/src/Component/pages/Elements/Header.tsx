ea56fcf0f2217ccc8e3e4636d2cfe1692ff6b3f6
AndrinRueegg <145564904+AndrinRueeggNoser@users.noreply.github.com>
2024-06-13 11:26:47 +0200

    fixxing header

--- my-app/src/Component/pages/Elements/Header.tsx
+++ my-app/src/Component/pages/Elements/Header.tsx
@@ -1,21 +1,20 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
-
+ 
 function Header() {
   return (
     <>
-    
-        <Toolbar className="header">
-          <div className="buttoncenter">
-            <Button color="inherit"> Reservation </Button>
-            <Button color="inherit"> Menu Cart </Button>
-            <Button color="inherit"> About Us</Button>
-            <Button className="button">Login</Button>
-          </div>
-        </Toolbar>
+      <Toolbar className="header">
+        <div className="buttoncenter">
+          <Button color="inherit"> Reservation </Button>
+          <Button color="inherit"> Menu Cart </Button>
+          <Button color="inherit"> About Us</Button>
+          <Button className="button">Login</Button>
+        </div>
+      </Toolbar>
     </>
   );
 }
-
+ 
 export default Header;
