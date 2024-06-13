5c4cf3823238d2c9c49c93de29ebf91cb5fe10b9
gabriel.arocha@edu.tbz.ch <145563844+Gabriel-A-NY@users.noreply.github.com>
2024-06-13 12:01:30 +0200

    update menupage

--- my-app/src/Component/pages/Menupage.tsx
+++ my-app/src/Component/pages/Menupage.tsx
@@ -1,26 +1,54 @@
-import {Grid,Table,TableCell,TableHead,TableRow,Typography,} from "@mui/material";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
+import React from "react";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 import Header from "./Elements/Header";
 
 function Menupage() {
+  const cardData = [
+    {
+      image:
+        "https://www.foodandwine.com/thmb/Wd4lBRZz3X_8qBr69UOu2m7I2iw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg",
+      title: "Pizza",
+      description: "Food",
+    },
+    {
+      image:
+        "https://www.foodandwine.com/thmb/Wd4lBRZz3X_8qBr69UOu2m7I2iw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg",
+      title: "Pizza",
+      description: "Food",
+    },
+  ]; // make dynamic json array to map over fetched data
+
   return (
     <>
       <Header />
-      <Grid container justifyContent="center">
-        <Grid item xs={12} sm={8} md={6}>
-          <h1>Menu</h1>
-        </Grid>
-        <Card sx={{ minWidth: 275 }}>
-          <CardContent>
-            <Typography variant="h5" component="div">
-              food
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              see all
-            </Typography>
-          </CardContent>
-        </Card>
+      <Typography variant="h1" gutterBottom>
+        Menu
+      </Typography>
+      <Grid container spacing={2}>
+        {cardData.map((card, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image={card.image}
+                title={card.title}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {card.description}
+                </Typography>
+                <Button size="small">See more</Button>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </>
   );
