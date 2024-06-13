ccf5962554a157ea5f2a6d2c2ec07f3a60c0b6ac
gabriel.arocha@edu.tbz.ch <145563844+Gabriel-A-NY@users.noreply.github.com>
2024-06-13 10:27:56 +0200

    make structure for menupage

--- my-app/src/Component/pages/Menupage.tsx
+++ my-app/src/Component/pages/Menupage.tsx
@@ -1,8 +1,31 @@
+import {
+  Grid,
+  Table,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+
 function Menupage() {
   return (
-    <>
-      <h1>Menupage</h1>
-    </>
+    <Grid container justifyContent="center">
+      <Grid item xs={12} sm={8} md={6}>
+        <h1>Menu</h1>
+      </Grid>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            food
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            see all
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 }
 
