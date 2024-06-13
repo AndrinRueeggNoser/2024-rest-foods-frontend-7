02f47d41778f1988187c98d0a0bb9410141236c5
AndrinRueegg <145564904+AndrinRueeggNoser@users.noreply.github.com>
2024-06-13 11:20:54 +0200

    fixxing css

--- my-app/src/Component/pages/Homepage.tsx
+++ my-app/src/Component/pages/Homepage.tsx
@@ -1,13 +1,20 @@
 import  Header  from "./Elements/Header";
-import React from "react";
+import Button from '@mui/material/Button';
 
 export default function HomePage() {
     return (
         <>
             <Header />
-            
-            <h1>RestFoods</h1>
-            
+            <h1>RestFoods</h1><div className="about">
+            <h2>Learn more about us</h2>
+            <p>On this Page you can leanr more about the history of our restaurant!.</p>
+                <Button>About Us</Button>
+                
+        </div><div className="menu">
+                <h2>Check out our menu</h2>
+                <p>Here you can see our Menu. We have food for everyone.</p>
+                <Button>Menu</Button>
+            </div>
         </>
     );
 }
