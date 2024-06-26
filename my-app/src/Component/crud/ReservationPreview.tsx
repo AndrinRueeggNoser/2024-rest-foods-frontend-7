31cf446da9826bf6e3f97e8ed11c3593261d3c6e
gabriel.arocha@edu.tbz.ch <145563844+Gabriel-A-NY@users.noreply.github.com>
2024-06-26 12:00:29 +0200

    make reservationpreview

--- my-app/src/Component/crud/ReservationPreview.tsx
+++ my-app/src/Component/crud/ReservationPreview.tsx
@@ -2,40 +2,60 @@ import { useEffect, useState } from "react";
 import MenuService from "../../service/MenuService";
 import { Button, Typography } from "@mui/material";
 import { useParams } from "react-router";
+import { ReservationProps } from "../ReservationProps";
+import Header from "../pages/Elements/Header";
 
 function ReservationPreview() {
- const [selectedDish, setSelectedDish] = useState<ReservationProps | null>(null);
- const { id } = useParams();
+  const [selectedReservation, setSelectedReservation] =
+    useState<ReservationProps | null>(null);
+  const { id } = useParams<{ id: string }>();
 
- useEffect(() => {
-   const fetchReservation = async () => {
-     try {
-       const response = await MenuService.getDishById(Number(id));
-       console.log(response);
-       setSelectedDish(response);
-     } catch (error) {
-       console.error(error);
-     }
-   };
+  useEffect(() => {
+    const fetchReservation = async () => {
+      try {
+        const response = await MenuService.getReservationById(Number(id));
+        console.log(response);
+        setSelectedReservation(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-   if (id) {
-     fetchReservation();
-   }
- }, [id]);
-
-
-  async function handleReservation() {
-    try {
-      await MenuService.getReservationById(Number(reservationId));
-    } catch (error) {
-      alert(error);
+    if (id) {
+      fetchReservation();
     }
-  }
+  }, [id]);
 
   return (
     <>
+      <div className="dashboard-background">
+        <Header />
+        <Typography variant="h1" gutterBottom>
+          Reservation Preview
+        </Typography>
 
-      
+        {selectedReservation ? (
+          <div key={selectedReservation.reservationId}>
+            <Typography variant="h5">
+              Reservation ID: {selectedReservation.reservationId}
+            </Typography>
+            <Typography variant="body1">
+              Name: {selectedReservation.firstName}
+            </Typography>
+            <Typography variant="body1">
+              Phone Number: {selectedReservation.lastName}
+            </Typography>
+            <Typography variant="body1">
+              Email: {selectedReservation.reservationTime}
+            </Typography>
+            <Typography variant="body1">
+              Date: {selectedReservation.personCount}
+            </Typography>
+          </div>
+        ) : (
+          <Typography variant="body1">Loading...</Typography>
+        )}
+      </div>
     </>
   );
 }
